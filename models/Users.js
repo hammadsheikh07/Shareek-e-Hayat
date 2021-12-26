392899837c1878a5573eb572aba6e00fe00846b2
Hamza Jamil <hamzajamil217@gmail.com>
2021-12-26 17:38:23 +0500

    profile picture attribute added in db

--- models/Users.js
+++ models/Users.js
@@ -2,40 +2,42 @@ const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
     name: {
-        type : String,
-        required : true
-       },
+        type: String,
+        required: true
+    },
     email: {
-        type : String,
-        required : true
+        type: String,
+        required: true
     },
     age:
     {
         type: Number,
-        required : true
+        required: true
     },
     City: {
-        type : String,
-        required : true
+        type: String,
+        required: true
     },
     Religion: {
-        type : String,
-        required : true
+        type: String,
+        required: true
     },
     Cast: {
-        type : String,
-        required : true
+        type: String,
+        required: true
     },
     password: {
-        type : String,
-        required : true
+        type: String,
+        required: true
     },
-    date: {
-        type : Date
+    profile_pic:
+    {
+        type: String,
+        required: true
     }
 })
 
 
-const User = mongoose.model('User' , UserSchema)
+const User = mongoose.model('User', UserSchema)
 
 module.exports = User;
